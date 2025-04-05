c389462be70993316f651dbf76515d7696a9c715
JonniUzb <joevega83574@gmail.com>
2025-04-06 06:05:19 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 # config.py
-TOKEN = "7067821205:AAFlfwdZhdhHUi-lqRpQF0Yo33Kr4nZDBqA"
+import os
+
+TOKEN = os.environ.get("TOKEN", "7067821205:AAFlfwdZhdhHUi-lqRpQF0Yo33Kr4nZDBqA")  # Render’da TOKEN bo‘lmasa, standart qiymat
